3b8ec1cbec1f576f9fcaa2545a781489816eb0c3
Pascal Krafft <krafft.pascal@gmail.com>
2018-04-25 15:51:54 +0200

    full functionality

--- js/app.js
+++ js/app.js
@@ -4,9 +4,15 @@
 let cardsList = document.querySelectorAll(".card");
 let cardsListArr = Array.from(cardsList);
 let cardsListArrShuffled;
+var cardOpen = [];
+let cardSelected;
+let moveCounter = 0;
+let foundPairs = 0;
 
 
 
+// intitial card shuffle
+restart();
 
 /*
  * Display the cards on the page
@@ -15,15 +21,14 @@ let cardsListArrShuffled;
  *   - add each card's HTML to the page
  */
 
-
-// New game restart
+// New game/ restart
 
 const restartListener = document.querySelector(".restart");
 restartListener.addEventListener("click", restart);
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-  array = cardsListArr;
+  // array = cardsListArr;
   var currentIndex = array.length,
     temporaryValue, randomIndex;
 
@@ -39,7 +44,20 @@ function shuffle(array) {
   return array;
 }
 
+function resetMoves() {
+  const moveSelector = document.querySelector(".moves");
+  moveCounter = 0;
+  moveSelector.innerHTML = moveCounter;
+}
+
+function resetCards(array) {
+  for (var i = 0; i < array.length; i++) {
+    array[i].setAttribute("class", "card");
+  }
+}
+
 function restart() {
+  resetCards(cardsList);
   cardsListArrShuffled = shuffle(cardsListArr);
   const deckNode = document.querySelector(".card").parentNode;
 
@@ -50,6 +68,7 @@ function restart() {
   for (i = 0; i < cardsListArr.length; i++) {
     deckNode.appendChild(cardsListArrShuffled[i]);
   }
+  resetMoves();
 }
 
 
@@ -65,3 +84,91 @@ function restart() {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+
+
+
+//add EventListener
+
+function cardsEventListener() {
+  // remove EventListener
+  $("li").off();
+  //add EventListener
+  $('.card').not(".open").not(".show").not(".match").not(".fa").on('click', open);
+};
+
+cardsEventListener();
+
+function moves() {
+  const moveSelector = document.querySelector(".moves");
+  ++moveCounter;
+  moveSelector.innerHTML = moveCounter;
+
+
+}
+
+
+
+function openCards(card) {
+
+  cardOpen.push(card);
+}
+
+function lockCards() {
+
+  cardOpen[0].setAttribute("class", "card match");
+  cardOpen[1].setAttribute("class", "card match");
+  cardOpen = [];
+  moves();
+  // activate cardsEventListener
+  cardsEventListener();
+
+
+}
+
+function removeCards() {
+  resetCards(cardOpen);
+  cardOpen = [];
+  moves();
+  // activate cardsEventListener
+  cardsEventListener();
+}
+
+
+
+function open() {
+
+
+  cardSelected = $(event.target);
+  cardSelected = cardSelected[0];
+  cardSelected.setAttribute("class", "card open show");
+  openCards(cardSelected);
+
+  cardsEventListener();
+  if (cardOpen.length === 2) {
+    // delete all cardListener
+    $("li").off();
+    if (cardOpen[0].childNodes[1].className === cardOpen[1].childNodes[1].className) {
+      lockCards();
+      ++foundPairs;
+
+
+      if (foundPairs === (cardsList.length / 2)) {
+
+        alert("you won");
+
+      }
+
+
+    } else {
+      setTimeout(function() {
+        removeCards();
+
+      }, 300);
+    }
+
+  }
+
+
+}
